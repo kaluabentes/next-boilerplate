09cc797bb218ff80873b91368524c98241cb4ac1
Kaluã Bentes <kaluanbentes@gmail.com>
2023-10-11 23:58:26 -0400

    refactor: add styled stuff to test

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -1,21 +1,30 @@
 import { fireEvent, render, screen } from "@testing-library/react"
 import { axe, toHaveNoViolations } from "jest-axe"
+import { ThemeProvider } from "styled-components"
 import "@testing-library/jest-dom"
+
+import defaultTheme from "@/styles/theme"
 
 import Button from "./Button"
 
 expect.extend(toHaveNoViolations)
 
+const ThemedButton = ({ onClick = () => "something" }) => (
+  <ThemeProvider theme={defaultTheme}>
+    <Button onClick={onClick}>Action</Button>
+  </ThemeProvider>
+)
+
 describe("Button", () => {
   it("should render without problems", async () => {
-    render(<Button>Action</Button>)
+    render(<ThemedButton />)
     expect(screen.getByText("Action")).toBeInTheDocument()
   })
 
   it("should interact without problems", async () => {
     const onClick = jest.fn()
 
-    render(<Button onClick={onClick}>Action</Button>)
+    render(<ThemedButton onClick={onClick} />)
 
     await fireEvent.click(screen.getByText("Action"))
 
@@ -23,7 +32,7 @@ describe("Button", () => {
   })
 
   it("should not have basic accessibility issues", async () => {
-    const { container } = render(<Button>Action</Button>)
+    const { container } = render(<ThemedButton />)
     const results = await axe(container)
     expect(results).toHaveNoViolations()
   })
